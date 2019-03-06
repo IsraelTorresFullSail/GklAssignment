cfc6a59edee61269535ffaef683d993c54d085d7
Israel Torres <israel.a.torres01@gmail.com>
2019-03-06 11:52:05 -0500

    update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,13 +14,21 @@ import { finalize } from 'rxjs/operators';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService, private storage: AngularFireStorage){}
+  constructor(private router: Router, private authService: AuthService, private storage: AngularFireStorage, private fb: FormBuilder){}
 
   @ViewChild('userName') inputUserName: ElementRef;
   @ViewChild('userImage') inputUserImage: ElementRef;
 
-  public email: string = '';
-  public password: string = '';
+  formRegister = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  //public email: string = '';
+  //public password: string = '';
+
+
 
   //-----Properties to save the picture url
   uploadPercent: Observable<number>;
@@ -39,7 +49,7 @@ export class RegisterComponent implements OnInit {
   }
 
   onAddUser(){
-    this.authService.registerUser(this.email, this.password)
+    this.authService.registerUser(this.formRegister.get('email').value, this.formRegister.get('password').value)
     .then((res) => {
       this.authService.isAuth().subscribe( user => {
         if(user){
